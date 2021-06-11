dc9374050a2871b61ed97c340974ed82bc20cc2e
AkhiroSantos <82278601+akhirosantos@users.noreply.github.com>
2021-06-11 12:42:16 -0500

    Mi primer Commit

---
+++ LPI_Semana11/LPI_Semana11/src/entidad/Cliente.java
@@ -0,0 +1,60 @@
+package entidad;
+
+import java.sql.Date;
+
+public class Cliente {
+	private int idCliente;
+	private String nombres;
+	private String apellidos;
+	private String dni;
+	private Date fechaNacimiento;
+	private TipoCliente idTipoCliente;
+	private String tipoClienteTexto;
+	
+	public int getIdCliente() {
+		tipoClienteTexto = idTipoCliente.getNombre();
+		return idCliente;
+	}
+	public void setIdCliente(int idCliente) {
+		this.idCliente = idCliente;
+	}
+	public String getNombres() {
+		return nombres;
+	}
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public TipoCliente getIdTipoCliente() {
+		return idTipoCliente;
+	}
+	public void setIdTipoCliente(TipoCliente idTipoCliente) {
+		this.idTipoCliente = idTipoCliente;
+	}
+	public String getTipoClienteTexto() {
+		return tipoClienteTexto;
+	}
+	public void setTipoClienteTexto(String tipoClienteTexto) {
+		this.tipoClienteTexto = tipoClienteTexto;
+	}
+	
+	
+	
+}
---
+++ LPI_Semana11/LPI_Semana11/src/entidad/TipoCliente.java
@@ -0,0 +1,22 @@
+package entidad;
+
+public class TipoCliente {
+	private int idTpoCliente;
+	private String nombre;
+	
+	public int getIdTpoCliente() {
+		return idTpoCliente;
+	}
+	public void setIdTpoCliente(int idTpoCliente) {
+		this.idTpoCliente = idTpoCliente;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+
+	
+}
